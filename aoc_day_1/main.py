b3a6d42a95c4dfdd935fbb745df134c8b10fa94f
alinazhark <alinasafina22@gmail.com>
2023-12-01 19:57:48 +0500

    addadd

--- aoc_day_1/main.py
+++ aoc_day_1/main.py
@@ -1,24 +1,18 @@
-f = open("/Users/alinasafina/PycharmProjects/pythonProject/aoc_day_1/input.txt", encoding="UTF-8")
-dict1 = {"one": 1, "two": 2, "three": 3, "four": 4, "five" : 5,
-         "six": 6, "seven": 7, "eight":8, "nine": 9}
+import re
+
+f = open("/Users/alinasafina/PycharmProjects/aoc2023/aoc_day_1/input.txt", encoding="UTF-8")
+numbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+           "six": 6, "seven": 7, "eight": 8, "nine": 9}
 result = 0
 for line in f:
-    firstDigit = ''
-    secondDigit = ''
-    indexOfFirstWord = 0
-    indexOfSecondWord = 0
-    indexOfFirstNumber = 0
-    indexOfSecondNumber = 0
-    for item, index in enumerate(line):
-        if item.isdigit() and firstDigit == '':
-            firstDigit = item
-            indexOfFirstNumber = index
-        elif item.isdigit() and firstDigit != '':
-            secondDigit = item
-            indexOfSecondNumber = index
+    for key in numbers:
+        index = line.find(key)
+        if index != -1:
+            line = line[:index+1]+str(numbers[key]) + line[index+1:]
+        index = line.rfind(key)
+        if index != -1:
+            line = line[:index+1]+str(numbers[key]) + line[index+1:]
+    digitsAll = re.findall(r'\d', line)
+    result += int(str(digitsAll[0]) + str(digitsAll[-1]))
 
-    if secondDigit == '':
-        result += int(firstDigit + firstDigit)
-    else:
-        result += int(firstDigit + secondDigit)
 print(result)
