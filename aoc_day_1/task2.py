1d6a29d6f4e7532717e28fe3b05463ce698fe50a
alinazhark <alinasafina22@gmail.com>
2023-12-01 20:10:04 +0500

    add first and second tasks

--- aoc_day_1/task2.py
+++ aoc_day_1/task2.py
@@ -5,14 +5,13 @@ numbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
            "six": 6, "seven": 7, "eight": 8, "nine": 9}
 result = 0
 for line in f:
+    print(line)
     for key in numbers:
         index = line.find(key)
         if index != -1:
             line = line[:index+1]+str(numbers[key]) + line[index+1:]
-        index = line.rfind(key)
-        if index != -1:
-            line = line[:index+1]+str(numbers[key]) + line[index+1:]
     digitsAll = re.findall(r'\d', line)
+    print(digitsAll[0], digitsAll[-1])
     result += int(str(digitsAll[0]) + str(digitsAll[-1]))
 
 print(result)
